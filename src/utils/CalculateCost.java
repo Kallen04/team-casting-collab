9475f2e842c428a6b152302cdd9b49c331248dcd
bubpasawatdurand <zeezee_x@hotmail.com>
2023-09-05 13:13:14 -0700

    Add method AvgCost

---
+++ src/utils/CalculateCost.java
@@ -0,0 +1,21 @@
+package utils;
+
+public class CalculateCost {
+
+
+    public double sumCost(double cost1, double cost2){
+
+        return cost1 + cost2;
+
+    }
+
+    public double AvgCost(double cost1, double cost2){
+
+        return (cost1+cost2)/2;
+    }
+
+
+
+
+
+}
