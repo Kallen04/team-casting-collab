9ce814a030fb020979bad63ad2351ed2125b3961
bubpasawatdurand <zeezee_x@hotmail.com>
2023-09-05 13:15:16 -0700

    Add comment

--- src/utils/CalculateCost.java
+++ src/utils/CalculateCost.java
@@ -2,13 +2,14 @@ package utils;
 
 public class CalculateCost {
 
-
+     // summery cost
     public double sumCost(double cost1, double cost2){
 
         return cost1 + cost2;
 
     }
 
+    // average cost
     public double AvgCost(double cost1, double cost2){
 
         return (cost1+cost2)/2;
