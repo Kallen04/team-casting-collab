c92a77a863f9a898a3ad3faac712294ee5b1758f
inciigin <iginincica@gmail.com>
2023-09-05 15:08:27 -0700

    Add ReverseString Method

--- src/utils/ReverseString.java
+++ src/utils/ReverseString.java
@@ -2,6 +2,15 @@ package utils;
 
 public class ReverseString {
 
+    public static int[] reverse(int[] array){
 
+        int[] reverse = new int[array.length];
+
+        for (int i = array.length - 1, j=0; i >= 0; i--) {
+            reverse[j++]= array[i];
+        }
+
+        return reverse;
+    }
 
 }
