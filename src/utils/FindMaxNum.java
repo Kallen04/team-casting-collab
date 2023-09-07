786f72c70649ecbe5d078385677a6b871e3c998c
bubpasawatdurand <zeezee_x@hotmail.com>
2023-09-07 11:54:50 -0700

    made static FindMaxNumber method

--- src/utils/FindMaxNum.java
+++ src/utils/FindMaxNum.java
@@ -2,13 +2,13 @@ package utils;
 
 public class FindMaxNum {
 
-    public int FindMaxNumber(int num1, int num2){
+    public static int FindMaxNumber(int num1, int num2){
         return (num1 > num2) ? num1 : num2;
     }
 
 
 
-    public double FindMaxNumber(double num1, double num2){
+    public static double FindMaxNumber(double num1, double num2){
         return (num1 > num2) ? num1 : num2;
     }
 
