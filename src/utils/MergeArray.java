0756fe9e4a72f215127576337f0555a82fc59ad3
Kristine Allen <kristine.allen04@gmail.com>
2023-09-07 11:45:25 -0700

    overload MergeArray.java

--- src/utils/MergeArray.java
+++ src/utils/MergeArray.java
@@ -16,4 +16,19 @@ public class MergeArray {
         }
         return arr3;
     }
+
+    public static double [] merge(double arr1 [], double arr2 []){
+
+        double [] arr3 = new double[arr1.length + arr2.length];
+
+        int k = 0;//new variable that will be used as an index number of merged array
+
+        for (int i = 0; i < arr1.length; i++, k++) {
+            arr3[k] = arr1[i];// every time i is increased, k is increased, we are assigning elements of arr1 to index numbers of arr3
+        }
+        for (int j = 0; j < arr2.length; j++, k++) {
+            arr3[k] = arr2[j];
+        }
+        return arr3;
+    }
 }
