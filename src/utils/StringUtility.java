32a8d32a4926074485de34ef35ab491749300e8a
gabrieladragoman <dragoman.gabriela@gmail.com>
2023-09-07 15:06:51 -0700

    Create nameFormatting() method

--- src/utils/StringUtility.java
+++ src/utils/StringUtility.java
@@ -11,5 +11,12 @@ public class StringUtility {
         return revers;
     }
 
+    public static String nameFormatting(String fullName) { // capitalization of the first and the last name
+        return fullName.substring(0, 1).toUpperCase() + fullName.substring(1, fullName.indexOf(" ")).toLowerCase()
+                + " " + fullName.substring(fullName.indexOf(" ") + 1, fullName.indexOf(" ") + 2).toUpperCase()
+                + fullName.substring(fullName.indexOf(" ") + 2).toLowerCase();
+    }
+
+
 
 }
