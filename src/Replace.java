72c4396515847a06cae3fd1cbf165530016bc367
Mahbuba <mahbuba.nasibova@yahoo.com>
2023-09-07 11:09:45 -0700

    create replace class

---
+++ src/Replace.java
@@ -0,0 +1,13 @@
+public class Replace{
+
+        public static void main(String[] args) {
+
+            String str = "xcodeX";
+
+            str = str.replace('x', 'a').replace('X', 'a');
+
+            System.out.println(str);
+
+        }
+
+}
