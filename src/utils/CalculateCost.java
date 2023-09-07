43f9a4f119151f1a7b1659536051bc3f7f9845bd
bubpasawatdurand <zeezee_x@hotmail.com>
2023-09-06 22:27:41 -0700

    made static method

--- src/utils/CalculateCost.java
+++ src/utils/CalculateCost.java
@@ -3,14 +3,14 @@ package utils;
 public class CalculateCost {
 
      // summery cost
-    public double sumCost(double cost1, double cost2){
+    public static double sumCost(double cost1, double cost2){
 
         return cost1 + cost2;
 
     }
 
     // average cost
-    public double AvgCost(double cost1, double cost2){
+    public static double AvgCost(double cost1, double cost2){
 
         return (cost1+cost2)/2;
     }
