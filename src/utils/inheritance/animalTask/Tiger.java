e5970f9309ac7f71a98db3f8cda738fbf7f01081
inciigin <iginincica@gmail.com>
2023-09-07 11:19:24 -0700

    Add Tiger Subclass with particular feature

---
+++ src/utils/inheritance/animalTask/Tiger.java
@@ -0,0 +1,10 @@
+package utils.inheritance.animalTask;
+
+public class Tiger extends Animal{ // Tiger Is An Animal
+
+
+    public void hunt(){
+        System.out.println(getName()+" is hunting");
+    }
+
+}
